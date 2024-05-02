58df3b798bc459c44c59a7bb29274eb4efd63596
shashi089 <shashidharnaik8@gmail.com>
2024-05-02 15:52:32 +0530

    server setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,32 @@
 import express from "express"
 import http from "http"
+import cors from "cors"
 
 
 const app = express()
-
+app.use(cors({
+    credentials:true,
+}))
 const server = http.createServer(app)
 
+/** Parse the body of the request */
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+/** Healthcheck */
+app.get('/ping', (req:express.Request, res:express.Response, next: express.NextFunction) => {
+    return res.status(200).json({ hello: 'world!' });
+});
+/** Error Handling */
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const error: Error = new Error('Not found');
+    res.status(404).json({
+        message: error.message
+    });
+});
+
+/** Server Listen */
 server.listen(8080,()=>{
-    console.log("Server Running")
+    console.log("Server is Running")
 })
