baba7338f36990c2e823b1855f11e5bb3544a57e
shashi089 <shashidharnaik8@gmail.com>
2024-05-03 10:41:01 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Wel Come to Socket Io</div>;
+  return <div className="App">Welcome to Socket Io</div>;
 }
 
 export default App;
